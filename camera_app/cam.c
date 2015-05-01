0dc1a082fb6d34e9893a88d4c88cbc48ceeb5f47
Deepti Kochar <deepti.kochar12@gmail.com>
2015-05-01 14:22:49 -0400

    Final commit

--- camera_app/cam.c
+++ camera_app/cam.c
@@ -1,14 +1,19 @@
-/* V4L2 video picture grabber
-   Copyright (C) 2009 Mauro Carvalho Chehab <mchehab@infradead.org>
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation version 2 of the License.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+/*
+ * Galileo Benchmark
+ *
+ * Description: This program controls the webcam connected to the CPU. 
+ *              A couple of samples of images are captured and processed
+ *              to save them as ppm images
+ *
+ * author:
+ *  Poorvaja Rajagopal
+ *
+ * Version: 1.0
+ * Date: 5/1/2015
+ *
+ *
+ * Version: 1.0
+ * Date: 5/1/2015
  */
 
 #include <stdio.h>
